b5a666ca30fdf43a117afe064423bb7449909123
Kadir <k1d12r04@gmail.com>
2022-06-18 13:07:58 +0300

    Take user move and generate game logic

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 "use strict";
+
+// Create an array of possible options which computer will choice one of them randomly
 const possibleOptions = ["Rock", "Paper", "Scissors"];
 
+// Take user's choice
+let playerMove = prompt("Please write your move: (Rock, Paper or Scissors) ");
+
+// Generate a random number to reveal computer's choice
 function randomNumber() {
   return Math.floor(Math.random() * 3);
 }
 
+// Random number created between 0 to 2 If 1 is created, the choice will be paper
+// which is placed second index in the "possibleOptions" array
 function computerPlay() {
-  console.log(possibleOptions[randomNumber()]);
+  return possibleOptions[randomNumber()];
 }
 
-computerPlay();
+// Main game function
+function playGame(playerSelection, computerSelection) {
+  // Take computer's choice which is generated randomly
+  computerPlay();
+
+  // Making case-insensitive the user's and computer's choice
+  let player = playerMove.toLowerCase();
+  let computer = computerPlay().toLowerCase();
+
+  // Game logic
+  if (
+    !player ||
+    player == " " ||
+    player != "paper" ||
+    player != "rock" ||
+    player != "scissors"
+  ) {
+    console.log("Please enter a valid move");
+    playGame();
+  } else if (computer === player) {
+    console.log("Tie game");
+  } else if ((computer === "rock") & (player === "scissors")) {
+    console.log("Computer won");
+  } else if ((computer === "paper") & (player === "rock")) {
+    console.log("Computer won");
+  } else if ((computer === "scissors") & (player === "paper")) {
+    console.log("Computer won");
+  } else {
+    console.log("Player won");
+  }
+}
+
+playGame();
